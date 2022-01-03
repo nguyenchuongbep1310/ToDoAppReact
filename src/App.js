b79239b8079fdd66aa677932a9796f4d860032d2
nguyenchuongbep1310 <67274198+nguyenchuongbep1310@users.noreply.github.com>
2022-01-03 22:07:49 +0700

    chuong

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("Todos")) ?? []
+  );
+  const [change, setChange] = useState(false);
+  const [id, setId] = useState();
+  const handleSubmit = () => {
+    if (change) {
+      handleupdate();
+    } else {
+      setTodos((prev) => {
+        const newTodos = [...prev, todo];
+        localStorage.setItem("Todos", JSON.stringify(newTodos));
+        return newTodos;
+      });
+      setTodo("");
+    }
+  };
+
+  const handleRemove = (index) => {
+    setTodos((pre) => {
+      const newTodos = pre.filter((item, id) => id !== index);
+      localStorage.setItem("Todos", JSON.stringify(newTodos));
+      return newTodos;
+    });
+  };
+  const handleupdate = () => {
+    todos[id] = todo;
+    setTodos((prev) => {
+      const newTodos = [...prev];
+      localStorage.setItem("Todos", JSON.stringify(newTodos));
+      return newTodos;
+    });
+    setChange(false);
+    setTodo("");
+  };
+  const handleChange = (index) => {
+    setChange(true);
+    setTodo(todos[index]);
+    setId(index);
+  };
+
+  console.log(change);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 30 }} className="Todo">
+      <input
+        value={todo}
+        onKeyDown={(e) => e.keyCode === 13 && todo && handleSubmit()}
+        onChange={(e) => {
+          setTodo(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          todo && handleSubmit();
+        }}
+      >
+        {(change && "Save") || "Add"}
+      </button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button
+              onClick={() => {
+                handleRemove(index);
+              }}
+              style={{ marginLeft: 30 }}
+            >
+              Remove
+            </button>
+            <button onClick={() => handleChange(index)}>changes</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
